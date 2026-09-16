199e8f1a26e0d615ad82cb96dc9a7c6b089699a7
Indra <muhammad26220@mail.unpad.ac.id>
2026-09-16 21:33:00 +0700

    edit a file

--- TugasEval2.cpp
+++ TugasEval2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
 int uang, kembalian;
 char no;
+char ulang;
 int lembar5, lembar1;
 cout<<"=== Vending Machine Automat ==="<<endl;
 cout<<"Pilihan :"<<endl;
@@ -13,69 +14,72 @@ cout<<"3. Kopi Susu (Rp 10000)"<<endl;
     do{
 cout<<"Pilih Kode Minuman (1-3): ";
 cin>>no;
-cout<<"Masukkan Uang Anda: ";
-cin>>uang;
-cout<<"=== Detail Transaksi === "<<endl;
 switch (no){
 case '1' :{
+cout<<"Masukkan Uang Anda: ";
+cin>>uang;
 if (uang<4000){
-    cout<<"Transaksi anda gagal!, uang anda kurang "<<4000-uang<<endl;
+cout<<"Transaksi anda gagal!, uang anda kurang "<<4000-uang<<endl;
 }
-
+    
 else{
     kembalian=uang-4000;
     lembar5=kembalian/5000;
     lembar1=(kembalian-lembar5*5000)/1000;
+cout<<"=== Detail Transaksi === "<<endl;
 cout<<"Transaksi anda berhasil!, uang kembalian anda adalah "<<kembalian<<endl;
 cout<<"Anda mendapat 5 ribu sebanyak "<<lembar5<<" lembar"<<endl;
 cout<<"Anda mendapat 1 ribu sebanyak "<<lembar1<<" lembar"<<endl;
 }
 }
     break;
-
+    
 case '2' :{
+    cout<<"Masukkan Uang Anda: ";
+cin>>uang;
 if (uang<6000){
-    cout<<"Transaksi anda gagal!, uang anda kurang "<<6000-uang<<endl;
+cout<<"Transaksi anda gagal!, uang anda kurang "<<6000-uang<<endl;
 }
-
+    
 else{
- kembalian=uang-6000;
+    kembalian=uang-6000;
     lembar5=kembalian/5000;
     lembar1=(kembalian-lembar5*5000)/1000;
+cout<<"=== Detail Transaksi === "<<endl;
 cout<<"Transaksi anda berhasil!, uang kembalian anda adalah "<<kembalian<<endl;
 cout<<"Anda mendapat 5 ribu sebanyak "<<lembar5<<" lembar"<<endl;
 cout<<"Anda mendapat 1 ribu sebanyak "<<lembar1<<" lembar"<<endl;;
 }
 }
     break;
-
-    case '3' :{
+    
+case '3' :{
+cout<<"Masukkan Uang Anda: ";
+cin>>uang;
 if (uang<10000){
-    cout<<"Transaksi anda gagal!, uang anda kurang "<<10000-uang<<endl;
+cout<<"Transaksi anda gagal!, uang anda kurang "<<10000-uang<<endl;
 }
-
+    
 else{
- kembalian=uang-10000;
+    kembalian=uang-10000;
     lembar5=kembalian/5000;
     lembar1=(kembalian-lembar5*5000)/1000;
+cout<<"=== Detail Transaksi === "<<endl;
 cout<<"Transaksi anda berhasil!, uang kembalian anda adalah "<<kembalian<<endl;
 cout<<"Anda mendapat 5 ribu sebanyak "<<lembar5<<" lembar"<<endl;
 cout<<"Anda mendapat 1 ribu sebanyak "<<lembar1<<" lembar"<<endl;
 }
 }
     break;
-
     
+    default:
+cout<<"Transaksi gagal, mohon pilih angka 1-3"<<endl; 
+}   
+cout<<"Lakukan transaksi lagi? (y/n): ";
+cin>>ulang;
 }
-    if(no<'1' || no>'3'){
-cout<<"Transaksi gagal, mohon pilih angka 1-3"<<endl;
-        
-    }
-    }
-
-while(no<'1' || no>'3');
-
-
-
+    
+    while(ulang=='y');
+    cout<<"Transaksi selesai";
     return 0;
 }
